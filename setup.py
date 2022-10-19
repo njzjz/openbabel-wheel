f6d5ec5ad390b979a31a831d0ddf32b56e946268
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-10-19 18:16:48 -0400

    add cross build argument to cmake

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
+import os
 import sys
 from pathlib import Path
 
 from cmake_build_extension import BuildExtension, CMakeExtension
 from setuptools import setup
 from distutils import sysconfig
+
+cmake_options = []
+# for arm 64 on MacOS
+if "macosx_arm64" in os.environ["CIBW_BUILD"]:
+    # cross build
+    cmake_options.append("-DCMAKE_SYSTEM_PROCESSOR=arm64")
 
 setup(
     ext_modules=[
@@ -20,6 +27,7 @@ setup(
                             "-DPYTHON_INCLUDE_PATH="+sysconfig.get_config_var('INCLUDEPY'),
                             "-DPYTHON_INCLUDE_DIR="+sysconfig.get_config_var('INCLUDEPY'),
                             "-DBUILD_BY_PIP=ON",
+                            *cmake_options,
                        ]),
     ],
     cmdclass=dict(build_ext=BuildExtension),
